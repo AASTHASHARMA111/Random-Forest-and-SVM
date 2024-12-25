034fe0a49620a3835315966cf5e0eef76cf319d4
AASTHASHARMA111 <saasthas111@gmail.com>
2024-12-26 02:02:53 +0530

    load dataset

--- code.py
+++ code.py
@@ -5,3 +5,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+
+# Load the datasets
+train_path = '/content/drive/MyDrive/ResoluteAI/Aastha Sharma Task 1/train.xlsx'
+test_path = '/content/drive/MyDrive/ResoluteAI/Aastha Sharma Task 1/test.xlsx'
+
+train_df = pd.read_excel(train_path)
+test_df = pd.read_excel(test_path)
